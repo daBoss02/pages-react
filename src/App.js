07854d2b0f5732a1cbc6900159fa09aa0fdb07fa
Seth Vandenbos <seth.vdbos@gmail.com>
2023-06-12 10:46:19 -0500

    update2

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import Documentation from './components/Documentation';
+import Tutorial from './components/Tutorials';
+import AboutUs from './components/AboutUs';
+import './style/index.css'
+
+function App() {
+  return (
+    <>
+    <Header />
+      <Routes>
+        <Route exact path='/' element={ <Home /> } />
+        <Route exact path='/documentation' element={ <Documentation /> } />
+        <Route exact path='/tutorials' element={ <Tutorial /> } />
+        <Route exact path='/aboutus' element={ <AboutUs />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
